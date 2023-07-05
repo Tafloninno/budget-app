223badec8cae4caacd559aacdf368ec951ebb8f5
Mugisha Innocent <111200646+Tafloninno@users.noreply.github.com>
2023-07-05 16:37:14 +0300

    Fix linters

--- Gemfile
+++ Gemfile
@@ -10,7 +10,6 @@ gem 'rubocop', '>= 1.0', '< 2.0'
 
 gem 'devise'
 
-
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem 'sprockets-rails'
 
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,11 +1,10 @@
 class Group < ApplicationRecord
-    validates :name, presence: true, length: { maximum: 25 }
-    validates :icon, presence: true
-  
-    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  
-    has_and_belongs_to_many :expenses, dependent: :destroy
+  validates :name, presence: true, length: { maximum: 25 }
+  validates :icon, presence: true
 
-  
-    has_one_attached :icon
-  end
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
+  has_and_belongs_to_many :expenses, dependent: :destroy
+
+  has_one_attached :icon
+end
